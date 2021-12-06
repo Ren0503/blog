1758961cf00b0e4b3d0b4af519f67fffd323bf90
Ren0503 <illuminati0440@gmail.com>
2021-12-06 14:26:06 +0700

    Add auth

--- server/src/user/service/user.service.ts
+++ server/src/user/service/user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../models/user.entity';
+import { Repository, Like } from 'typeorm';
+import { User, UserRole } from '../models/user.interface';
+import { Observable, from, throwError } from 'rxjs';
+import { switchMap, map, catchError } from 'rxjs/operators';
+import { AuthService } from 'src/auth/service/auth.service';
+import { paginate, Pagination, IPaginationOptions } from 'nestjs-typeorm-paginate';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
+        private authService: AuthService
+    ) { }
+
+    findOne(id: number): Observable<User> {
+        return from(this.userRepository.findOne({ id }, { relations: ['blogEntries'] })).pipe(
+            map((user: User) => {
+                const { password, ...result } = user;
+                return result;
+            })
+        )
+    }
+}
